93af899ec54974b938fe49cc84fc4d919d7b889b
raoyuandong <837215079@qq.com>
2018-04-25 17:34:08 +0800

    Update myjqGrid.js

--- laravel/public/js/myjqGrid.js
+++ laravel/public/js/myjqGrid.js
@@ -127,7 +127,9 @@
                     },
                     editurl: "http://www.lar.test.com/",//nothing is saved
                     caption: "测试表格",
-                    autowidth: true,
+                    //autowidth: true,
+                    shrinkToFit:false,  
+                    autoScroll: true,  
             });
 
             $(window).triggerHandler('resize.jqGrid');//trigger window resize to make the grid get the correct size
