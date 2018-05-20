55780ff2e5a587475af38555e4bada09663013f1
raoyuandong <837215079@qq.com>
2018-05-20 13:19:33 +0800

    调试

--- laravel/routes/channels.php
+++ laravel/routes/channels.php
@@ -14,3 +14,6 @@
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+Broadcast::channel('notice', function () {
+  return true; //这个里面可以写授权的逻辑判断，我这里直接返回true了，不做验证
+});
