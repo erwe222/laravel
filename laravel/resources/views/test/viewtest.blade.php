c8a2c9343c06f503cce03f4601c455e928f1e685
raoyuandong <837215079@qq.com>
2018-05-20 12:55:41 +0800

    调试

--- laravel/resources/views/test/viewtest.blade.php
+++ laravel/resources/views/test/viewtest.blade.php
@@ -1,7 +1,8 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
-<script src="/js/app.js"></script>
+
 
 <script src="//{{ Request::getHost() }}:6001/socket.io/socket.io.js"></script>
+<script src="/js/app.js"></script>
 <script>
     console.log(window.Echo);
 </script>
