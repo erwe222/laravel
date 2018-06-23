294bf30f498e0f4ff22d85ba3a656ce24aa607f5
raoyuandong <837215079@qq.com>
2018-06-23 12:59:25 +0800

    调试

--- laravel/app/Http/Controllers/weixin/WeixinController.php
+++ laravel/app/Http/Controllers/weixin/WeixinController.php
@@ -25,8 +25,9 @@ class WeixinController extends CController{
             $this->weChatApiClass->valid();
         }
 
-//        $messageXml = isset($GLOBALS ["HTTP_RAW_POST_DATA"]) ? $GLOBALS ["HTTP_RAW_POST_DATA"] : file_get_contents("php://input");
-//        $message = var_export($GLOBALS,true);
+        $messageXml = isset($GLOBALS ["HTTP_RAW_POST_DATA"]) ? $GLOBALS ["HTTP_RAW_POST_DATA"] : file_get_contents("php://input");
+        $message = var_export($GLOBALS,true);
+        \Log::info($message);
         \Log::info('收到消息');
         if($this->weChatApiClass->checkSignature()){
             \Log::info('消息验证成功');
