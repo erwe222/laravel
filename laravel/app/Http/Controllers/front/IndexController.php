14386a2196c45a06a35a6fd483052db868f6b033
raoyuandong <837215079@qq.com>
2018-06-23 14:53:08 +0800

    阿什顿发

--- laravel/app/Http/Controllers/front/IndexController.php
+++ laravel/app/Http/Controllers/front/IndexController.php
@@ -14,7 +14,7 @@ class IndexController extends CController{
      */
     public function index(){
           $url = 'https://repair.sh-jinger.com/front/wx/auth2';
-          $this->WeChatApiClass->getWeChatAuthCode($url,123);
+          $this->WeChatApiClass->getWeChatAuthCode($url,123,false);
         
 //        return view('frontend.index.index');
     }
