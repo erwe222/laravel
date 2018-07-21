9edab51ff794be48d65ecd96ab0fcab30f51fda9
raoyuandong <837215079@qq.com>
2018-07-21 11:44:24 +0800

    阿什顿发

--- laravel/resources/views/weixin/auth/register.blade.php
+++ laravel/resources/views/weixin/auth/register.blade.php
@@ -59,7 +59,7 @@
                   </div>
                   <div class="login-form">		
                     <input type="password" placeholder="请填写验证码" style="font-size: 18px;background-color: transparent;width: 50%;">	
-                    <div style="width:90px;background:red;float:right;text-align: center;line-height:25px;border-radius:15px;color:#fff;background: #FFA366;padding-left: 5px;padding-right:5px;">发送短信(20s)</div>
+                    <div style="width:90px;background:red;float:right;text-align: center;line-height:30px;border-radius:15px;color:#fff;background: #FFA366;padding-left: 5px;padding-right:5px;">发送短信(20s)</div>
                   </div>
                   <input type="submit" class=" btn-login" value="立即注册">				 			  
                 </form>
